775ecbcba5513bbf353644e89412f6033a087518
sivajeetsabdakar <133634489+sivajeetsabdakar@users.noreply.github.com>
2024-09-14 22:44:09 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -9,7 +9,6 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# Function to get the gemini response
 def get_gemini_response(input, image):
     model = genai.GenerativeModel('gemini-1.5-flash')
     if input != "":
@@ -18,33 +17,26 @@ def get_gemini_response(input, image):
         response = model.generate_content(image)
     return response.text
 
-# Streamlit page configuration
 st.set_page_config(page_title="AI Image Chatbot")
 
-st.header("I'm your study buddy :)")
+st.header("I'm your non-blind study buddy :)")
 
-# Load chat history from session state
 if 'history' not in st.session_state:
     st.session_state['history'] = []
 
-# Input field for text and file upload for image
-input = st.text_input("What is your doubt?: ", key="input")
+input = st.text_input("What's your doubt?: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Initialize image variable
 image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-# Create two side-by-side columns for "Send" button and "Chat History" dropdown
 col1, col2 = st.columns([3, 1])
 
-# Submit button
 with col1:
     submit = st.button("Send")
 
-# Chat history dropdown (expander)
 with col2:
     with st.expander("Chat History"):
         if st.session_state['history']:
@@ -52,16 +44,16 @@ with col2:
                 st.write(f"**You**: {chat['input']}")
                 st.write(f"**StudyBuddy**: {chat['response']}")
 
-# When the submit button is clicked
+if st.button("Clear History"):
+    st.session_state['history'] = []
+
 if submit:
     response = get_gemini_response(input, image)
     
-    # Store the question and response in session state
     st.session_state['history'].append({
         'input': input,
         'response': response
     })
     
-    # Display the response
     st.subheader("The Response is")
     st.write(response)
